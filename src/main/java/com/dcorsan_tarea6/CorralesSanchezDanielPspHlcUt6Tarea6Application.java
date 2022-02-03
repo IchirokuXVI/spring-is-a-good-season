02cca1f660a1ba6860246d6abb858abfe8dabfd1
Ichiroku <danieliyo65@gmail.com>
2022-02-03 10:22:13 +0100

    Spring project generated

---
+++ src/main/java/com/dcorsan_tarea6/CorralesSanchezDanielPspHlcUt6Tarea6Application.java
@@ -0,0 +1,13 @@
+package com.dcorsan_tarea6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorralesSanchezDanielPspHlcUt6Tarea6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorralesSanchezDanielPspHlcUt6Tarea6Application.class, args);
+	}
+
+}
