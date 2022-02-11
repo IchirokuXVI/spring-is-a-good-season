afa4f650443bc32131330c9e6d23b731619624a6
Ichiroku <danieliyo65@gmail.com>
2022-02-11 09:18:05 +0100

    Added a simple way to check data at startup

--- src/main/java/com/dcorsan/tarea6/CorralesSanchezDanielPspHlcUt6Tarea6Application.java
+++ src/main/java/com/dcorsan/tarea6/CorralesSanchezDanielPspHlcUt6Tarea6Application.java
@@ -11,6 +11,12 @@ import com.dcorsan.tarea6.services.AdmissionServiceI;
 import com.dcorsan.tarea6.services.DoctorServiceI;
 import com.dcorsan.tarea6.services.PatientServiceI;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
@@ -41,6 +47,12 @@ public class CorralesSanchezDanielPspHlcUt6Tarea6Application implements CommandL
 		
 		for (Admission a : admissionService.getAll()) {
 			System.out.println(a);
+		}
+		
+		patientService.add(new Patient("KONO", "Dio", "Da", "Coffin", "Italy", "25565", "666666666", new SimpleDateFormat("yyyy-MM-dd").parse("2000-31-01"), new ArrayList<>()));
+		
+		for (Patient p : patientService.getAll()) {
+			System.out.println(p);
 		}
 	}
 
